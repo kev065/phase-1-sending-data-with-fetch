805bea785d74d11181501d49446e1f99b23db85e
KELVIN MURITHI <kelvinmurithi@live.com>
2023-10-03 10:41:31 +0300

    test 1 done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your code here
+function submitData (name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name, email
+        })
+    }
